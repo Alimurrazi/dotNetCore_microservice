5de162bee8e6a7020b93add5fa353a56b10d187b
Alimurrazi <alimurrazi570@gmail.com>
2020-10-18 10:31:43 +0600

    redis db connection add

---
+++ src/Basket/Basket.API/Data/BasketContext.cs
@@ -0,0 +1,20 @@
+﻿using Basket.API.Data.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Data
+{
+    public class BasketContext: IBasketContext
+    {
+        private readonly ConnectionMultiplexer _redisConnectin;
+        public IDatabase Redis { get; }
+        public BasketContext(ConnectionMultiplexer redisConnectin)
+        {
+            _redisConnectin = redisConnectin;
+            Redis = redisConnectin.GetDatabase();
+        }
+    }
+}
